a6bbdd48733549a9d6b87a7e713f47fb9b66a001
CruiseM21 <Gahzehl1@aol.com>
2017-11-19 23:16:20 -0500

    Add files via upload

---
+++ MyNameIsApp.java
@@ -0,0 +1,18 @@
+/*
+ * Vincent Christaldi
+ * My project to display
+ * My Name Is<>, My Favorite Color Is<>, If I Won The Lottery, I Would <***>
+ */
+package mynameisapp;
+
+public class MyNameIsApp {
+
+    public static void main(String[] args)
+    {
+        System.out.println("My name is: Vincent Christaldi");
+        System.out.println("My favorite color is: Green");
+        System.out.println("If I won the lottery, I would do what I could to help my family");
+    }
+    
+    
+}
